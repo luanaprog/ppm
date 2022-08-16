ef6bbee4232b4519ed44c4f23dae8a8d36d6f01c
Luana Bez Batti <progmobileluana@gmail.com>
2022-08-16 19:41:29 -0300

    Botão Limpar

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelector('#clique-aqui').onclick = () => {
+    const nome = document.querySelector('#campo-nome').value; 
+    if(nome){
+        alert(`Hello world, ${nome}!`);
+    }
+};
+
+document.querySelector('#limpar').onclick = () => {
+    document.querySelector(`#campo-nome`).value = '';
+}
